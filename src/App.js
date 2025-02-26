140e9b15dc5812ff0e06e6c3ac506485b086dcad
lnsohu <35254544+lnsohu@users.noreply.github.com>
2025-02-26 23:17:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login'; // 登录页面组件
-import RestaurantList from './pages/RestaurantAdmin'; // 餐厅列表页面
-import RestaurantAdmin from './pages/RestaurantAdmin';
+import RestaurantList from './pages/RestaurantList'; // 餐厅列表页面
 
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -32,17 +31,6 @@ const App = () => {
                     element={
                         isLoggedIn ? (
                             <RestaurantList /> // 如果已登录，显示餐厅列表
-                        ) : (
-                            <Navigate to="/" /> // 如果未登录，跳转到登录页面
-                        )
-                    }
-                />
-                {/* 添加餐厅页面 */}
-                <Route
-                    path="/add-restaurant"
-                    element={
-                        isLoggedIn ? (
-                            <RestaurantAdmin /> // 如果已登录，显示添加餐厅页面
                         ) : (
                             <Navigate to="/" /> // 如果未登录，跳转到登录页面
                         )
