a990e85e4779d1dd4e18fb87a71e474620a0d56b
lnsohu <35254544+lnsohu@users.noreply.github.com>
2025-04-21 15:30:22 +0800

    Update Menu.js

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../supabase';
+import { supabase } from '../supabaseClient';
 import { useParams } from 'react-router-dom';
 
 const Menu = () => {
